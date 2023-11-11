ce03db043bd800085c69ce7add75fbb3eee2133c
RigelOrion251 <54441806+RigelOrion251@users.noreply.github.com>
2023-11-11 16:41:16 -0500

    cleanup two programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagEasyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagEasyTest.java
@@ -32,6 +32,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testBed_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testBed_Linear.java
@@ -32,9 +32,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 
 /*
@@ -83,16 +81,17 @@ public class testBed_Linear extends LinearOpMode {
 
             // Setup a variable for each drive wheel to save power level for telemetry
             double liftPower;
-         //   double rightPower;
+         /*
+               double rightPower;
+             Choose to drive using either Tank Mode, or POV Mode
+             Comment out the method that's not used.  The default below is POV.
+             POV Mode uses left stick to go forward, and right stick to turn.
+             - This uses basic math to combine motions and is easier to drive straight.
+                        double drive = -gamepad1.left_stick_y;
+                        double turn  =  gamepad1.right_stick_x;
+                        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            */
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-//            double drive = -gamepad1.left_stick_y;
-//            double turn  =  gamepad1.right_stick_x;
-//            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
             liftPower = gamepad1.left_stick_y;
 
             // Tank Mode uses one stick to control each wheel.
